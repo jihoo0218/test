2a2f77039de3aa715c3adc28a57b1672d946b2b2
redjoon@pinecni.com <redjoon@pinecni.com>
2025-02-05 17:04:14 +0900

    비밀번호 암호화

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -5,7 +5,9 @@ import com.example.demo.mapper.AccountMapper;
 import com.example.demo.vo.AccountVO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.apache.commons.codec.digest.Crypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -13,7 +15,14 @@ import java.util.Map;
 @Service
 public class AccountService {
     @Autowired AccountMapper accountMapper;
+
+    @Value("${password-crypt-key}")
+    private String encodingSalt;
+
     public void join(AccountVO accountVO) {
+        String encodedPassword = Crypt.crypt(accountVO.getPassword(), encodingSalt);
+        accountVO.setPassword(encodedPassword);
+
         accountMapper.join(accountVO);
     }
 
@@ -23,6 +32,7 @@ public class AccountService {
             throw new IllegalStateException("일치하는 정보가 없습니다.");
         }
         String password = loginForm.getPassword();
+        password = Crypt.crypt(password,encodingSalt);
         if(!joinedAccount.get("password").equals(password)){
             throw new IllegalStateException("일치하는 정보가 없습니다");
         }
